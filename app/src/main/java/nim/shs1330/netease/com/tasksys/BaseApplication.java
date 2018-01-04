25b6bfe69a03e3b091173aa56448dd081e13d634
寻乐人 <291490299@qq.com>
2018-01-04 10:07:02 +0800

    dex的apk加固

--- app/src/main/java/nim/shs1330/netease/com/tasksys/BaseApplication.java
+++ app/src/main/java/nim/shs1330/netease/com/tasksys/BaseApplication.java
@@ -2,16 +2,6 @@ package nim.shs1330.netease.com.tasksys;
 
 import android.app.Application;
 import android.content.Context;
-import android.util.Log;
-
-import java.io.IOException;
-import java.lang.reflect.InvocationTargetException;
-
-import nim.shs1330.netease.com.tasksys.dynamic_hook.Hook;
-import nim.shs1330.netease.com.tasksys.dynamic_hook.classloader.ClassLoaderHelper;
-import nim.shs1330.netease.com.tasksys.dynamic_hook.json.JSONParser;
-import nim.shs1330.netease.com.tasksys.helper.Client;
-import nim.shs1330.netease.com.tasksys.helper.FileHelper;
 
 /**
  * Created by shs1330 on 2017/10/11.
@@ -24,54 +14,52 @@ public class BaseApplication extends Application {
     @Override
     public void onCreate() {
         super.onCreate();
-        Client.init(getApplicationContext());
-
-        FileHelper.extractAssets(PluginOne);
-        FileHelper.extractAssets(DexFile);
-//        File dexFile = getFileStreamPath(PluginOne);
-//        File optDexFile = getFileStreamPath("app-debug.dex");
-//        ClassLoaderHelper.hookParentClassLoader(getClassLoader(), dexFile, optDexFile);
-
-        try {
-            JSONParser.parser();
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-
-
-
-        try {
-            ClassLoaderHelper.hookCustomClassLoader(getFileStreamPath(PluginOne));
-            ClassLoaderHelper.hookDexClassloader(getFileStreamPath(DexFile));
-        } catch (ClassNotFoundException e) {
-            e.printStackTrace();
-        } catch (NoSuchMethodException e) {
-            e.printStackTrace();
-        } catch (InvocationTargetException e) {
-            e.printStackTrace();
-        } catch (IllegalAccessException e) {
-            e.printStackTrace();
-        } catch (NoSuchFieldException e) {
-            e.printStackTrace();
-        } catch (InstantiationException e) {
-            e.printStackTrace();
-        }
-        //Client.init(getApplicationContext());
-        Log.d(TAG, "onCreate: " + "source apk install");
+//        Client.init(getApplicationContext());
+//
+//        FileHelper.extractAssets(PluginOne);
+//        FileHelper.extractAssets(DexFile);
+////        File dexFile = getFileStreamPath(PluginOne);
+////        File optDexFile = getFileStreamPath("app-debug.dex");
+////        ClassLoaderHelper.hookParentClassLoader(getClassLoader(), dexFile, optDexFile);
+//
+//        try {
+//            JSONParser.parser();
+//        } catch (IOException e) {
+//            e.printStackTrace();
+//        }
+//
+//        try {
+//            ClassLoaderHelper.hookCustomClassLoader(getFileStreamPath(PluginOne));
+//            ClassLoaderHelper.hookDexClassloader(getFileStreamPath(DexFile));
+//        } catch (ClassNotFoundException e) {
+//            e.printStackTrace();
+//        } catch (NoSuchMethodException e) {
+//            e.printStackTrace();
+//        } catch (InvocationTargetException e) {
+//            e.printStackTrace();
+//        } catch (IllegalAccessException e) {
+//            e.printStackTrace();
+//        } catch (NoSuchFieldException e) {
+//            e.printStackTrace();
+//        } catch (InstantiationException e) {
+//            e.printStackTrace();
+//        }
+//        //Client.init(getApplicationContext());
+//        Log.d(TAG, "onCreate: " + "source apk install");
     }
 
     @Override
     protected void attachBaseContext(Context base) {
         super.attachBaseContext(base);
-        //hookInstrumentation
-        Client.getHelper(Hook.class).hook();
-        //hook ClipBoard IBinder
-        Client.getHelper(Hook.class).hookBinder();
-        //hook ActivityManagerService
-        Client.getHelper(Hook.class).hookAMS();
-        //hook PackageManagerService
-        Client.getHelper(Hook.class).hookPMS();
-        //hook ActivityThread H mCallback
-        Client.getHelper(Hook.class).hookHandler();
+//        //hookInstrumentation
+//        Client.getHelper(Hook.class).hook();
+//        //hook ClipBoard IBinder
+//        Client.getHelper(Hook.class).hookBinder();
+//        //hook ActivityManagerService
+//        Client.getHelper(Hook.class).hookAMS();
+//        //hook PackageManagerService
+//        Client.getHelper(Hook.class).hookPMS();
+//        //hook ActivityThread H mCallback
+//        Client.getHelper(Hook.class).hookHandler();
     }
 }
--- app/src/main/java/nim/shs1330/netease/com/tasksys/MainActivity.java
+++ app/src/main/java/nim/shs1330/netease/com/tasksys/MainActivity.java
@@ -1,26 +1,17 @@
 package nim.shs1330.netease.com.tasksys;
 
-import android.app.Fragment;
-import android.app.FragmentTransaction;
 import android.content.ComponentName;
-import android.content.Intent;
 import android.content.ServiceConnection;
 import android.os.Bundle;
 import android.os.Handler;
 import android.os.IBinder;
-import android.os.Looper;
 import android.os.Message;
-import android.os.RemoteException;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
-import android.view.View;
-
-import java.lang.reflect.InvocationTargetException;
-import java.lang.reflect.Method;
+import android.util.Log;
 
 import nim.shs1330.netease.com.tasksys.binder.Component;
 import nim.shs1330.netease.com.tasksys.binder.ComponentNative;
-import nim.shs1330.netease.com.tasksys.dynamic_hook.classloader.ClassLoaderHelper;
 
 /**
  * Created by shs1330 on 2017/10/10.
@@ -34,106 +25,107 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        bindService(new Intent(this, MainService.class), serviceConnection, BIND_AUTO_CREATE);
-        new Handler(Looper.getMainLooper()).postDelayed(new Runnable() {
-            @Override
-            public void run() {
-                try {
-                    component.printProcessName(1000);
-                } catch (RemoteException e) {
-                    e.printStackTrace();
-                }
-            }
-        }, 2000);
-
-        findViewById(R.id.bt_showTask).setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                //startActivity(new Intent(MainActivity.this, TargetActivity.class));
-                try {
-                    Class helloC = ClassLoaderHelper.sDexClassloader.loadClass("Hello");
-                    Method myPrintM = helloC.getDeclaredMethod("MyPrint", String.class);
-                    myPrintM.invoke(null, "zhang !!");
-
-                    Method mainM = helloC.getDeclaredMethod("main", String[].class);
-                    mainM.invoke(null, new Object[]{ new String[]{"String"}});
-                } catch (ClassNotFoundException e) {
-                    e.printStackTrace();
-                } catch (NoSuchMethodException e) {
-                    e.printStackTrace();
-                } catch (InvocationTargetException e) {
-                    e.printStackTrace();
-                } catch (IllegalAccessException e) {
-                    e.printStackTrace();
-                }
-            }
-        });
-
-        findViewById(R.id.bt_clear).setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent intent = new Intent();
-                intent.setComponent(new ComponentName("nim.shs1330.netease.com.plugintwp",
-                        "nim.shs1330.netease.com.plugintwp.MainActivity"));
-                startActivity(intent);
-            }
-        });
-
-        findViewById(R.id.bt_task1).setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                FragmentTransaction transaction = getFragmentManager().beginTransaction();
-                Fragment fragment = null;
-                StubFragment stubFragment = null;
-                try {
-                    ClassLoader classLoader = ClassLoaderHelper.getPluginClassLoader("nim.shs1330.netease.com.plugintwp");
-                    Class fragmentClazz = classLoader.loadClass("nim.shs1330.netease.com.plugintwp.MainFragment");
-                    fragment = (Fragment) fragmentClazz.newInstance();
-
-                    stubFragment = new StubFragment();
-                    stubFragment.setRemoteFragment(fragment);
-
-                    Method setProxyFragmentF = fragmentClazz.getDeclaredMethod("setProxyFragment", Fragment.class);
-                    setProxyFragmentF.setAccessible(true);
-                    setProxyFragmentF.invoke(fragment, stubFragment);
-
-                } catch (ClassNotFoundException e) {
-                    e.printStackTrace();
-                } catch (InstantiationException e) {
-                    e.printStackTrace();
-                } catch (IllegalAccessException e) {
-                    e.printStackTrace();
-                } catch (NoSuchMethodException e) {
-                    e.printStackTrace();
-                } catch (InvocationTargetException e) {
-                    e.printStackTrace();
-                }
-                transaction.replace(R.id.flyt_contrainer, stubFragment);
-                transaction.commit();
-            }
-        });
-
-        findViewById(R.id.bt_task2).setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent intent = new Intent();
-                intent.setAction("plugtwo");
-                intent.putExtra("text", "ZZZ");
-                sendBroadcast(intent);
-            }
-        });
-
-        findViewById(R.id.bt_task3).setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent intent = new Intent();
-                intent.setComponent(new ComponentName("nim.shs1330.netease.com.plugintwp",
-                        "nim.shs1330.netease.com.plugintwp.PluginService"));
-                startService(intent);
-            }
-        });
-
-        thread.start();
+        Log.d(TAG, "onCreate:  src MainActivity created");
+//        bindService(new Intent(this, MainService.class), serviceConnection, BIND_AUTO_CREATE);
+//        new Handler(Looper.getMainLooper()).postDelayed(new Runnable() {
+//            @Override
+//            public void run() {
+//                try {
+//                    component.printProcessName(1000);
+//                } catch (RemoteException e) {
+//                    e.printStackTrace();
+//                }
+//            }
+//        }, 2000);
+//
+//        findViewById(R.id.bt_showTask).setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View v) {
+//                //startActivity(new Intent(MainActivity.this, TargetActivity.class));
+//                try {
+//                    Class helloC = ClassLoaderHelper.sDexClassloader.loadClass("Hello");
+//                    Method myPrintM = helloC.getDeclaredMethod("MyPrint", String.class);
+//                    myPrintM.invoke(null, "zhang !!");
+//
+//                    Method mainM = helloC.getDeclaredMethod("main", String[].class);
+//                    mainM.invoke(null, new Object[]{ new String[]{"String"}});
+//                } catch (ClassNotFoundException e) {
+//                    e.printStackTrace();
+//                } catch (NoSuchMethodException e) {
+//                    e.printStackTrace();
+//                } catch (InvocationTargetException e) {
+//                    e.printStackTrace();
+//                } catch (IllegalAccessException e) {
+//                    e.printStackTrace();
+//                }
+//            }
+//        });
+//
+//        findViewById(R.id.bt_clear).setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View v) {
+//                Intent intent = new Intent();
+//                intent.setComponent(new ComponentName("nim.shs1330.netease.com.plugintwp",
+//                        "nim.shs1330.netease.com.plugintwp.MainActivity"));
+//                startActivity(intent);
+//            }
+//        });
+//
+//        findViewById(R.id.bt_task1).setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View v) {
+//                FragmentTransaction transaction = getFragmentManager().beginTransaction();
+//                Fragment fragment = null;
+//                StubFragment stubFragment = null;
+//                try {
+//                    ClassLoader classLoader = ClassLoaderHelper.getPluginClassLoader("nim.shs1330.netease.com.plugintwp");
+//                    Class fragmentClazz = classLoader.loadClass("nim.shs1330.netease.com.plugintwp.MainFragment");
+//                    fragment = (Fragment) fragmentClazz.newInstance();
+//
+//                    stubFragment = new StubFragment();
+//                    stubFragment.setRemoteFragment(fragment);
+//
+//                    Method setProxyFragmentF = fragmentClazz.getDeclaredMethod("setProxyFragment", Fragment.class);
+//                    setProxyFragmentF.setAccessible(true);
+//                    setProxyFragmentF.invoke(fragment, stubFragment);
+//
+//                } catch (ClassNotFoundException e) {
+//                    e.printStackTrace();
+//                } catch (InstantiationException e) {
+//                    e.printStackTrace();
+//                } catch (IllegalAccessException e) {
+//                    e.printStackTrace();
+//                } catch (NoSuchMethodException e) {
+//                    e.printStackTrace();
+//                } catch (InvocationTargetException e) {
+//                    e.printStackTrace();
+//                }
+//                transaction.replace(R.id.flyt_contrainer, stubFragment);
+//                transaction.commit();
+//            }
+//        });
+//
+//        findViewById(R.id.bt_task2).setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View v) {
+//                Intent intent = new Intent();
+//                intent.setAction("plugtwo");
+//                intent.putExtra("text", "ZZZ");
+//                sendBroadcast(intent);
+//            }
+//        });
+//
+//        findViewById(R.id.bt_task3).setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View v) {
+//                Intent intent = new Intent();
+//                intent.setComponent(new ComponentName("nim.shs1330.netease.com.plugintwp",
+//                        "nim.shs1330.netease.com.plugintwp.PluginService"));
+//                startService(intent);
+//            }
+//        });
+//
+//        thread.start();
     }
 
 
